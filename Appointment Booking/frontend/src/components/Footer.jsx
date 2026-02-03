f314983723e569de6f6bedfe8354f2572423cf2b
MarcuSS002 <hyperxaxis@gmail.com>
2026-02-03 11:12:35 +0530

    Added new razor keys

--- Appointment Booking/frontend/src/components/Footer.jsx
+++ Appointment Booking/frontend/src/components/Footer.jsx
@@ -1,45 +1,50 @@
-
-import { assets } from '../assets/assets'
+import { assets } from "../assets/assets";
 
 const Footer = () => {
-    return (
-        <div className='md:mx-10'>
-            <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-10 my-10 mt-40 text-sm'>
-                {/* ---------Left Side------- */}
-                <div>
-                    <img className='mb-5 w-40' src={assets.logo} alt="" />
-                    <p className='w-full md:w-2/3 text-gray-600 leading-6'>
-                   Prescripto is a web application that helps users track and manage their health easily.
-It allows users to monitor key health data, view reports, and stay updated about their health status in one place.
-The system aims to make health tracking simple, organized, and accessible anytime, helping users take better care of themselves.
-                    </p>
-                </div>
-                {/* ---------Center------- */}
-                <div>
-                    <p className='text-xl font-medium mb-5'>Company</p>
-                    <ul className='flex flex-col gap-2 text-gray-600'>
-                        <li>Home</li>
-                        <li>About us</li>
-                        <li>Contact us</li>
-                        <li>Privacy policy</li>
-                    </ul>
-                </div>
-                {/* ---------Right Side------- */}
-                <div>
-                    <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
-                    <ul className='flex flex-col gap-2 text-gray-600'>
-                        <li>+0-000-000-000</li>
-                        <li>harshgautam333@gmail.com</li>
-                    </ul>
-                </div>
-            </div>
-            {/* {Copyright Section} */}
-            <div>
-                <hr />
-                <p className='py-5 text-sm text-center'>Copyright {new Date().getFullYear()} @ Harsh's.dev - All Rights Reserved.</p>
-            </div>
+  return (
+    <div className="md:mx-10">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-10 my-10 mt-40 text-sm">
+        {/* ---------Left Side------- */}
+        <div>
+          <img className="mb-5 w-40" src={assets.logo} alt="" />
+          <p className="w-full md:w-2/3 text-gray-600 leading-6">
+            Prescripto is a web application that helps users track and manage
+            their health easily. It allows users to monitor key health data,
+            view reports, and stay updated about their health status in one
+            place. The system aims to make health tracking simple, organized,
+            and accessible anytime, helping users take better care of
+            themselves.
+          </p>
         </div>
-    )
-}
+        {/* ---------Center------- */}
+        <div>
+          <p className="text-xl font-medium mb-5">Company</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
+            <li>Home</li>
+            <li>About us</li>
+            <li>Contact us</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+        {/* ---------Right Side------- */}
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-2 text-gray-600">
+            <li>+0-000-000-000</li>
+            <li>harshgautam333@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+      {/* {Copyright Section} */}
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright {new Date().getFullYear()} @ Harsh's.dev - All Rights
+          Reserved.
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
--- Appointment Booking/frontend/src/components/SpecialityMenu.jsx
+++ Appointment Booking/frontend/src/components/SpecialityMenu.jsx
@@ -1,22 +1,36 @@
-
-import { specialityData } from '../assets/assets'
-import { Link } from 'react-router-dom'
+import { specialityData } from "../assets/assets";
+import { Link } from "react-router-dom";
 
 const SpecialityMenu = () => {
   return (
-    <div className='flex flex-col items-center gap-4 py-16 text-gray-800' id='speciality'>
-      <h1 className='text-3xl font-medium'>Find by Speciality</h1>
-      <p className='text-center text-sm'>Simply browse through our extensive list of trusted doctors,<br /> schedule your appointment hassle-free.</p>
-      <div className='flex gap-4 w-full sm:justify-center pt-5 overflow-scroll'>
+    <div
+      className="flex flex-col items-center gap-4 py-16 text-gray-800"
+      id="speciality"
+    >
+      <h1 className="text-3xl font-medium">Find by Speciality</h1>
+      <p className="text-center text-sm">
+        Simply browse through our extensive list of trusted doctors,
+        <br /> schedule your appointment hassle-free.
+      </p>
+      <div className="flex gap-4 w-full sm:justify-center pt-5 overflow-scroll">
         {specialityData.map((item, index) => (
-          <Link onClick={()=> scrollTo(0,0)} className='flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-20px] transition-all duration-300' key={index} to={`/doctors/${item.speciality}`}>
-            <img className='block mx-auto w-20 h-20 sm:w-24 sm:h-24 object-contain rounded-full mb-2' src={item.image} alt="" />
-            <p className='text-center'>{item.speciality}</p>
+          <Link
+            onClick={() => scrollTo(0, 0)}
+            className="flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-20px] transition-all duration-300"
+            key={index}
+            to={`/doctors/${item.speciality}`}
+          >
+            <img
+              className="block mx-auto w-20 h-20 sm:w-24 sm:h-24 object-contain rounded-full mb-2"
+              src={item.image}
+              alt=""
+            />
+            <p className="text-center">{item.speciality}</p>
           </Link>
         ))}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SpecialityMenu
+export default SpecialityMenu;
